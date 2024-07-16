4f341e1d3c611fe18c04da0454705e7cc7440c91
iamsharks <127649227+iamsharks@users.noreply.github.com>
2024-07-16 20:32:51 +0530

    Delete Print 2D array.py

--- Print 2D array.py
+++
@@ -1,8 +0,0 @@
-n,m=map(int,input().split())
-arr=[]
-for i in range(n):
-    row=list(map(int,input().split()))
-    arr.append(row)
-for i in range(n):
-    for j in range(n-i):
-        print(*arr[i])
